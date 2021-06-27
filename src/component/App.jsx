068ab0733aee280ee1f156a9daa3e9f60c100189
Nimanita <mamatasingh2507@gmail.com>
2021-06-27 22:05:20 +0530

    Files added

---
+++ src/component/App.jsx
@@ -0,0 +1,119 @@
+
+import './App.css';
+import Img2 from '../images/img-12.jfif';
+import Img1 from '../images/img-2.jfif';
+import Profile from '../images/profile-1.jpg';
+import Three from '../images/more-horizontal.svg';
+import Insta from '../images/instagram.svg';
+import Twit from '../images/twitter.svg';
+import Face from '../images/facebook.svg';
+import {useState} from 'react';
+function App() {
+ 
+  const[classes,changeclass]=useState({
+      hover : "box1",
+      mainbox : "cold-lg-6 cold-sm-12 cold-md-12 col1n",
+      hoverimg : "img",
+      visi : "row invisi"
+  });
+  const[classes2,changeclass2]=useState({
+    hover : "box1 boxs",
+    mainbox : "cold-lg-6 cold-sm-12 cold-md-12 col1n",
+    hoverimg : "img",
+    visi : "row invisi"
+});
+  function mouseinside(x){
+   
+    if(x === "card1")
+    {
+        changeclass({
+           ...classes,
+           hover : "box2",
+          mainbox : "cold-lg-6 cold-sm-12 cold-md-12 col2n",
+           hoverimg : "imgh",
+          visi : "row visi"
+        });
+    }
+    else
+    {
+      changeclass2({
+        ...classes2,
+        hover : "box2",
+       mainbox : "cold-lg-6 cold-sm-12 cold-md-12 col2n",
+        hoverimg : "imgh",
+       visi : "row visi"
+     });
+    }
+
+  }
+  function mouseoutside(x){
+    if(x === "card1")
+    {
+        changeclass({
+           ...classes,
+           hover : "box1",
+          mainbox : "cold-lg-6 cold-sm-12 cold-md-12 col1n",
+           hoverimg : "img",
+          visi : "row invisi"
+        });
+    }
+    else
+    {
+      changeclass2({
+        ...classes2,
+        hover : "box1",
+       mainbox : "cold-lg-6 cold-sm-12 cold-md-12 col1n",
+        hoverimg : "img",
+       visi : "row invisi"
+     });
+    }
+  }
+  return (
+    <div className="container App">
+      <div class="row  approw">
+        <div class={classes.mainbox} onMouseEnter={() =>mouseinside("card1") }
+        onMouseLeave={() =>mouseoutside("card1") }>
+        <img class={classes.hover} src={Img2}/>
+        <div class={classes.visi}>
+        <img src={Face} class="icon" />
+        <img src={Insta} class="icon"/>
+        <img src={Twit} class="icon"/>
+        </div>
+        <div class="d-flex flex-row content">
+        
+         <img src={Profile} class={classes.hoverimg}/>
+         
+          <div class="details">
+          <h1 class="title">Avneesh</h1>
+          <h4 class="post">Manager</h4>
+          </div>
+          <img src={Three} class=" img3 ml-auto"/>
+        </div>
+        </div>
+        <div class={classes2.mainbox} onMouseEnter={() =>mouseinside("card2") }
+        onMouseLeave={() =>mouseoutside("card2") }>
+        <img class={classes2.hover} src={Img1}/>
+        <div class={classes2.visi}>
+        <img src={Face} class="icon" />
+        <img src={Insta} class="icon"/>
+        <img src={Twit} class="icon"/>
+        </div>
+        <div class="d-flex flex-row content">
+        
+         <img src={Profile} class={classes2.hoverimg}/>
+         
+          <div class="details">
+          <h1 class="title">Avneesh</h1>
+          <h4 class="post">Manager</h4>
+          </div>
+          <img src={Three} class=" img3 ml-auto"/>
+        </div>
+        </div>
+       
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
